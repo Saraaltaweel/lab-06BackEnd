80cbf83935b98eb56276b10633b4265fe94cb072
Saraaltaweel <s.bata97.st@gmail.com>
2021-03-30 23:45:48 +0300

    lab7

--- server.js
+++ server.js
@@ -2,32 +2,47 @@
 
 
 require('dotenv').config();
-
+const superagent = require('superagent');
 const express = require('express');
 const cors = require('cors');
 
 const PORT = process.env.PORT ||3000;
+const GEOCODE_API_KEY= process.env.GEOCODE_API_KEY;
+const WEATHERS_API_KEY= process.env.WEATHERS_API_KEY;
+const PARK_API_KEY= process.env.PARK_API_KEY;
 const app = express();
 app.use(cors());
 
 app.get('/location', handelLocationRequest);
 app.get('/weather', handelWeatherRequest);
+// app.get('/parks', handelParksRequest);
 
 function handelLocationRequest(req, res) {
 
     const searchQuery = req.query.city;
-  if (!searchQuery){
-    res.status(500).send('something is wrong in server');}
+    const url=`https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${searchQuery}&format=json`;
+
+    if (!searchQuery){
+        res.status(500).send('something is wrong in server');}
+
+    superagent.get(url).then(item=>{
+        const location = new Location(searchQuery,item.body[0])
+        res.send(location);
+    })
+
   
   
-    const locationsRawData = require('./data/location.json');
-    const location = new Location(locationsRawData[0])
-    res.send(location);
+    
 }
 
 function handelWeatherRequest(req, res) {
-    const weatherRawData = require('./data/weather.json');
-    const weatherData = [];
+    const searchQuery = req.query.city;
+    const url = `http://api.weatherbit.io/v2.0/forecast/daily?KEY=${WEATHERS_API_KEY}&city=${searchQuery}&country=US`;
+  
+    superagent.get(url).then(item=>{
+        console.log(item.body);
+
+    })
 
     weatherRawData.data.forEach(weather => {
         weatherData.push(new Weather(weather));
@@ -38,7 +53,8 @@ function handelWeatherRequest(req, res) {
 }
 
 
-function Location(data) {
+function Location(searchQuery,data) {
+    this.searchQuery=searchQuery;
     this.formatted_query = data.display_name;
     this.latitude = data.lat;
     this.longitude = data.lon;
